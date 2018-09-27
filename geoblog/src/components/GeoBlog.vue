d7e0cc3d1299f799d2814b1c00fb0ecdfc47d37a
quasim <nicomaxa@gmail.com>
2018-09-27 11:28:18 +0200

    Chapter06 Some routing

---
+++ geoblog/src/components/GeoBlog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="geo-blog">
+    <!-- More to come -->
+  </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "GeoBlog"
+    }
+</script>
+
+<style scoped>
+
+</style>
